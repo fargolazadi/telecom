29b7d00d51a7ef94636f312cd4a8598ab18999e4
fargolazadi <fargol.azadi1@gmail.com>
2024-02-27 17:10:21 +0330

    media
    i opened media file and set that

--- telecom/settings.py
+++ telecom/settings.py
@@ -110,6 +110,8 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
@@ -118,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 static_root = BASE_DIR/'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT =BASE_DIR /'media'
+
 STATICFILES_DIRS = [ BASE_DIR /'statics',]
 
 
