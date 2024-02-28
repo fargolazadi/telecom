c2b3f6c42ad561d0d98a497dbd7d9c69e8b3c164
fargolazadi <fargol.azadi1@gmail.com>
2024-02-28 18:01:33 +0330

    header
    i made header (about,contact),

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,6 @@ from mainapp.views import *
 app_name='mainapp'
 urlpatterns=[
 path('',index_views,name='index'),
-path('about',about_views,name='about')
+path('about/',about_views,name='about'),
+path('contact/',contact_views,name='contact')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def index_views(request):
     return render(request,'index2-rtl.html')
+
+
+def about_views(request):
+    return render(request,'about-us-21.html')
+
+
+def contact_views(request):
+    return render(request,'contact-us-2.html')
